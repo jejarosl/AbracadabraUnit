395a97a840dac7f49b2ecf315c910cdf4c43ed17
Jekaterina Jaroslavceva <dmivit98@gmail.com>
2022-04-13 12:40:32 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Statistics from './components/Statistics';
+import Inventory from './components/Inventory';
+import Manufactures from './components/Manufactures';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Statistics />} />
+        <Route path="/inventories/:kod" element={<Inventory />} />   
+        <Route path="/manufactures" element={<Manufactures/>} />
+      </Routes>
+    </Router>
   );
 }
 
